a2b5a85d8706fbc88893e8db45585d6cccfa7f8c
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2024-06-11 02:11:29 -0400

    fixed history functionality

--- main.c
+++ main.c
@@ -7,103 +7,104 @@
 #define INPUT_MAX 100
 #define HIST_MAX 15
 
-
-struct node{
-	char cmd[INPUT_MAX];
-	struct node *next;
+struct node {
+    char cmd[INPUT_MAX];
+    struct node *next;
 };
 typedef struct node node;
 
-struct queue{
-	int count;
-	node *head;
-	node *tail;
+struct queue {
+    int count;
+    node *head;
+    node *tail;
 };
 typedef struct queue queue;
 
 queue *q;
 
-void q_init(){
-	q = malloc(sizeof(queue));
-	q->count = 0;
-	q->head = NULL;
-	q->tail = NULL;
+void q_init() {
+    q = malloc(sizeof(queue));
+    q->count = 0;
+    q->head = NULL;
+    q->tail = NULL;
 }
 
-int is_empty(){
-	return q->tail == NULL;
+int is_empty() {
+    return q->tail == NULL;
 }
 
-int get_size(){
-	return q->count;
+int get_size() {
+    return q->count;
 }
 
-void call_init(){	
-	clear();
-	printf("Welcome to my shell!\n");
-	printf("Use this shell at your own risk!!\n\n");
-	q_init();
+void call_init() {
+    clear();
+    printf("Welcome to my shell!\n");
+    printf("Use this shell at your own risk!!\n\n");
+    q_init();
 }
 
-void enqueue(char *buffer){
-	node *tmp = malloc(sizeof(node));
-	strcpy(buffer, tmp->cmd);
+void enqueue(char *buffer) {
+    node *tmp = (node*)malloc(sizeof(node));
+    tmp->next = NULL;
 
-	if(!is_empty()){
-		q->tail->next = tmp;
-		q->tail = tmp;
-	}
-	else{
-		q->head = q->tail = tmp;
-	}
-	q->count++;
+    strcpy(tmp->cmd, buffer);
+    if (is_empty()) {
+        q->head = q->tail = tmp;
+    } else {
+        q->tail->next = tmp;
+        q->tail = tmp;
+    }
+    q->count++;
 }
 
-void add_history(char *buffer){
-	enqueue(buffer);		
+void add_history(char *buffer) {
+    enqueue(buffer);
 }
 
-void print_history(){
-	node *tmp;
+void print_history() {
+    node *tmp;
 
-	if(q->count == 0)
-		printf("History is empty!\n");
-	else{
-		tmp = q->head;
-		while(tmp != NULL){
-			printf("%s\n", tmp->cmd);
-			tmp = tmp->next;
-		}
-	}
+    if (q->count == 0)
+        printf("History is empty!\n");
+    else {
+        tmp = q->head;
+        while (tmp != NULL) {
+            printf("%s", tmp->cmd);
+            tmp = tmp->next;
+        }
+    }
 }
 
-int read_input(char *input){
-	char *buffer = (char*) malloc (sizeof(input));
-	memcpy(buffer, input, strlen(input));
-	
-	if (strlen(buffer) == 0)
-		return 0;
-	add_history(buffer);
-	return 1;
+int read_input(char *input) {
+    char *buffer = (char*) malloc (strlen(input) + 1);
+    strcpy(buffer, input);
+
+    if (strlen(buffer) == 0) {
+        free(buffer);
+        return 0;
+    }
+    add_history(buffer);
+    free(buffer);  // Free the buffer after adding to history
+    return 1;
 }
 
-int main(){
-	
-	char input[INPUT_MAX];
-	char *username;
-	call_init();
+int main() {
+    char input[INPUT_MAX];
+    char *username;
+    call_init();
 
-	username = getlogin();
-	while(1){
-		printf("%s:>>", username);
-		fgets(input, INPUT_MAX, stdin);
-		if (strcmp(input, "exit\n") == 0)
-			break;
-		if (read_input(input)){
-			if (strcmp(input, "history\n") == 0){
-				print_history();
-			}
-		}	
-	}
-	return 0;
+    username = getlogin();
+    while (1) {
+        printf("%s:>>", username);
+        fgets(input, INPUT_MAX, stdin);
+        if (strcmp(input, "exit\n") == 0)
+            break;
+        if (read_input(input)) {
+            if (strcmp(input, "history\n") == 0) {
+                print_history();
+            }
+        }
+    }
+    return 0;
 }
